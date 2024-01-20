4a8373228ad12fde14a701b9e813df1c9d1b07fd
Jeonghoo Son <80911999+jhson929@users.noreply.github.com>
2024-01-20 19:23:05 +0900

    Update Top Earners.sql

--- Aggregation/Top Earners.sql
+++ Aggregation/Top Earners.sql
@@ -4,3 +4,9 @@ FROM employee
 GROUP BY total_earnings
 ORDER BY total_earnings DESC
 LIMIT 1
+
+-- 서브쿼리로도 풀 수 있다.
+SELECT (SELECT MAX(salary*months) from employee)
+     , COUNT(*)
+FROM employee
+WHERE salary*months = (SELECT MAX(salary*months) from employee)
