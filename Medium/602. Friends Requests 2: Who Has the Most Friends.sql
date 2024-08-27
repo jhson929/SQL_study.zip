2ae512f5e58abea18daf3304d0a56a32beb6c689
Jeonghoo Son <80911999+jhson929@users.noreply.github.com>
2024-08-27 13:49:35 +0900

    Update 602. Friends Requests 2: Who Has the Most Friends.sql

--- Medium/602. Friends Requests 2: Who Has the Most Friends.sql
+++ Medium/602. Friends Requests 2: Who Has the Most Friends.sql
@@ -1,3 +1,4 @@
+-- 방법1
 WITH FriendsInfo AS (
     SELECT requester_id AS id
     ,COUNT(*) AS num
@@ -20,3 +21,20 @@ WITH FriendsInfo AS (
 SELECT id, num
 FROM SUB
 WHERE num = (SELECT MAX(num) FROM SUB)
+
+-- 방법2
+WITH BASE AS (
+    SELECT requester_id id
+    FROM RequestAccepted
+
+    UNION ALL
+
+    SELECT accepter_id id
+    FROM RequestAccepted
+)
+
+SELECT id, COUNT(*) num
+FROM BASE
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1 -- most friends가 1명임을 보장하므로 이 코드 또한 가능.
